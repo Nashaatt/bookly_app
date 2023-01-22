1f31c3629e4e67f318df021434fa1dee7556b9b0
nashaatt <kemom903@gmail.com>
2023-01-22 03:22:21 +0200

    adding animation to splash screen

--- lib/features/presentation/views/widgets/spalsh_view_body.dart
+++ lib/features/presentation/views/widgets/spalsh_view_body.dart
@@ -1,8 +1,41 @@
 import 'package:bookly_app/core/ulties/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin{
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        vsync: this ,
+        duration: const Duration(
+        seconds: 1,
+    ));
+
+    slidingAnimation =
+        Tween<Offset>(begin: Offset(0,10) , end: Offset.zero).animate(animationController);
+
+    animationController.forward();
+    slidingAnimation.addListener(() {
+      setState(() {
+
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +45,13 @@ class SplashViewBody extends StatelessWidget {
            crossAxisAlignment: CrossAxisAlignment.stretch,
            children: [
                 Image.asset(Assets.logo),
-                const SizedBox(height: 20.0,),
-                const Text(
-                    'Read Free Books',
-                  textAlign: TextAlign.center,
+                const SizedBox(height: 5.0,),
+                SlideTransition(
+                  position: slidingAnimation,
+                  child: const Text(
+                      'Read Free Books',
+                    textAlign: TextAlign.center,
+                  ),
                 ),
            ],
          ),
